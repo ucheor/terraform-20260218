c2e88796773fd63b7c098681cc3131d8749b7c74
miriamora <miriamoranu@gmail.com>
2026-02-19 19:07:44 -0700

    added script to VM EC@ and key pair

--- sg.tf
+++ sg.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["173.183.236.105"]
+    cidr_blocks = ["173.183.236.105/32"]
   }
 
   egress {
