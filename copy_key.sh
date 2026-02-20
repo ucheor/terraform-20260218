d65cca8cf8fc5809a1be250a5b0452499eed589e
miriamora <miriamoranu@gmail.com>
2026-02-20 16:39:42 -0700

    adding provisioner

--- copy_key.sh
+++ copy_key.sh
@@ -5,12 +5,3 @@ echo "${key_content}" > /home/ubuntu/${key_name}.pem
 chown ubuntu:ubuntu /home/ubuntu/${key_name}.pem
 chmod 400 /home/ubuntu/${key_name}.pem
 
-# #!/bin/bash
-# # Write the private key content to the file
-# cat << 'EOF' > /home/ubuntu/key.pem
-# ${private_key}
-# EOF
-
-# # Set permissions
-# chown ubuntu:ubuntu /home/ubuntu/key.pem
-# chmod 400 /home/ubuntu/key.pem
